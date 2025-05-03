11086f2186c8fc80d5f5f352010782bfb6dafc61
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2025-05-04 00:02:53 +0700

    Create MataKuliah.java

---
+++ MataKuliah.java
@@ -0,0 +1,23 @@
+public class MataKuliah {
+    private String kode;
+    private String nama;
+    private int sks;
+
+    public MataKuliah() {
+        this.kode = "TI23C";
+        this.nama = "Pemrograman Berbasis Objek";
+        this.sks = 3;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getSks() {
+        return sks;
+    }
+}
