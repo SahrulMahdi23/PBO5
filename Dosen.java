c67c8e593cf88d84785bdaeb055286007d0115d2
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2025-05-04 00:00:06 +0700

    Create Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,23 @@
+public class Dosen {
+    private String nama;
+    private String nip;
+    private String fakultas;
+
+    public Dosen() {
+        this.nama = "Bapak Yulhan";
+        this.nip = "123456789";
+        this.fakultas = "Teknologi Informatika";
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public String getFakultas() {
+        return fakultas;
+    }
+}
